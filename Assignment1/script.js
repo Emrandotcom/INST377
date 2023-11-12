d0df30cc8ef3adb99d3090d4efdb10cff20dce87
Emran <63017060+Emrandotcom@users.noreply.github.com>
2023-11-12 16:45:01 -0500

    another commit

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -1,32 +1,29 @@
 function playGame() {
-    // Choices for the game
+
     const choices = ["rock", "paper", "scissors"];
     
-    // Get user's choice
     const userChoice = document.getElementById("userChoice").value;
     document.getElementById("userChoiceBox").textContent = "You Chose: " + userChoice;
 
-    // Get computer's random choice
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
     document.getElementById("computerChoiceBox").textContent = "Computer Chose: " + computerChoice;
 
-    // Determine the winner
     if (
         (userChoice === "rock" && computerChoice === "scissors") ||
         (userChoice === "paper" && computerChoice === "rock") ||
         (userChoice === "scissors" && computerChoice === "paper")
     ) {
         alert("You Win!");
-        document.body.style.backgroundColor = "#2ecc71"; // Green background
+        document.body.style.backgroundColor = "#2ecc71";
     } else if (
         (computerChoice === "rock" && userChoice === "scissors") ||
         (computerChoice === "paper" && userChoice === "rock") ||
         (computerChoice === "scissors" && userChoice === "paper")
     ) {
         alert("You Lose!");
-        document.body.style.backgroundColor = "#e74c3c"; // Red background
+        document.body.style.backgroundColor = "#e74c3c";
     } else {
         alert("It's a Tie!");
-        document.body.style.backgroundColor = "#3498db"; // Blue background
+        document.body.style.backgroundColor = "#3498db";
     }
 }
